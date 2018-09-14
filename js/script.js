31e98454f04d5f85e9f13d1cc5a7e3d7a18f6cfc
Karolina Madej <kkarolinamadej@gmail.com>
2018-09-14 19:59:57 +0200

    Add transition to menu, fix header RWD

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-//
-// // hide h1
-//
-// let navbarToggler = document.querySelector('.navbar-toggler');
-// let introduction = document.querySelector('.introduction-box');
-//
+
+// hide h1
+
+let navbarToggler = document.querySelector('.navbar-toggler');
+let introduction = document.querySelector('.introduction-box');
+
 // navbarToggler.addEventListener('click', () => {
 //   if (introduction.style.display === "none") {
 //     introduction.style.display = "block";
@@ -11,3 +11,11 @@
 //     introduction.style.display = "none";
 //   }
 // });
+
+navbarToggler.addEventListener('click', () => {
+  if (introduction.style.opacity === "0") {
+    introduction.style.opacity = "1";
+  } else {
+    introduction.style.opacity = "0";
+  }
+});
