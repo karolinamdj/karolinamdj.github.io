1d6da6ffab259c5e3d17bdf3220ba89e50061768
Karolina Madej <kkarolinamadej@gmail.com>
2018-09-14 18:15:56 +0200

    Add contact and footer sections

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-
-// hide h1
-
-let navbarToggler = document.querySelector('.navbar-toggler');
-let introduction = document.querySelector('.introduction-box');
-
-navbarToggler.addEventListener('click', () => {
-  if (introduction.style.display === "none") {
-    introduction.style.display = "block";
-  } else {
-    introduction.style.display = "none";
-  }
-});
+//
+// // hide h1
+//
+// let navbarToggler = document.querySelector('.navbar-toggler');
+// let introduction = document.querySelector('.introduction-box');
+//
+// navbarToggler.addEventListener('click', () => {
+//   if (introduction.style.display === "none") {
+//     introduction.style.display = "block";
+//   } else {
+//     introduction.style.display = "none";
+//   }
+// });
