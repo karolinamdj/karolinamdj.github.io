37704f4f4a0a34272c9d81209d330e98c237539b
Karolina Madej <kkarolinamadej@gmail.com>
2018-09-20 18:22:18 +0200

    Add links underlines and background slide

--- js/script.js
+++ js/script.js
@@ -4,14 +4,6 @@
 let navbarToggler = document.querySelector('.navbar-toggler');
 let introduction = document.querySelector('.introduction-box');
 
-// navbarToggler.addEventListener('click', () => {
-//   if (introduction.style.display === "none") {
-//     introduction.style.display = "block";
-//   } else {
-//     introduction.style.display = "none";
-//   }
-// });
-
 navbarToggler.addEventListener('click', () => {
   if (introduction.style.opacity === "0") {
     introduction.style.opacity = "1";
@@ -19,3 +11,15 @@ navbarToggler.addEventListener('click', () => {
     introduction.style.opacity = "0";
   }
 });
+
+// slide header background image
+
+let bgImage = document.querySelector('.header');
+let lastScrollTop = 0;
+let bgY = 75;
+
+window.addEventListener('scroll', () => {
+  let st = window.pageYOffset || document.documentElement.scrollTop;
+  let bgY = st == 0 ? 75 : 75 - (st * 0.04);
+  bgImage.style.backgroundPosition = `50% ${bgY}%`;
+}, false);
